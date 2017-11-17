ee3e16c0ad1951d30ca4e8915622eda7fae9a847
CarletonCalvinLam <32558238+CarletonCalvinLam@users.noreply.github.com>
2017-11-17 08:24:38 -0500

    Test Code of Hardware
    no printout reading, only pass or fail

--- Test/MPU_6050_TEST/MPU_6050_TEST.ino
+++ Test/MPU_6050_TEST/MPU_6050_TEST.ino
@@ -5,6 +5,7 @@
 #include<Wire.h>
 const int MPU_addr = 0x68; // I2C address of the MPU-6050
 int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+int16_t OldAcX, OldAcY,OldAcZ,OldGyX,OldGyY,OldGyZ;//reference values to test against
 void setup() {
   Wire.begin();
   Wire.beginTransmission(MPU_addr);
@@ -13,7 +14,19 @@ void setup() {
   Wire.endTransmission(true);
   Serial.begin(9600);
 }
-void loop() {
+Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true); // request a total of 14 registers
+  OldAcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  OldAcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  OldAcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  OldGyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  OldGyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  OldGyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+delay (100)
+void GetAll() {
   Wire.beginTransmission(MPU_addr);
   Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
   Wire.endTransmission(false);
@@ -25,12 +38,43 @@ void loop() {
   GyX = Wire.read() << 8 | Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
   GyY = Wire.read() << 8 | Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ = Wire.read() << 8 | Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-  Serial.print("AcX = "); Serial.print(AcX);
+  /*Serial.print("AcX = "); Serial.print(AcX);
   Serial.print(" | AcY = "); Serial.print(AcY);
   Serial.print(" | AcZ = "); Serial.print(AcZ);
   Serial.print(" | Tmp = "); Serial.print(Tmp / 340.00 + 36.53); //equation for temperature in degrees C from datasheet
   Serial.print(" | GyX = "); Serial.print(GyX);
   Serial.print(" | GyY = "); Serial.print(GyY);
   Serial.print(" | GyZ = "); Serial.println(GyZ);
-  delay(100);
+  delay(100);*/
 }
+System.out.println("Tilt Forward and Hold until further notice");
+GetAll();
+if (GyZ<=OldGyZ||AcZ<=OldAcZ){
+  System.out.println("Test Failed");
+}
+else{
+  System.out.println("Tilt Backward and Hold until further notice");
+  GetAll();
+  if (GyZ>=OldGyZ||AcZ>=OldAcZ){
+    System.out.println("Test Failed");
+  }
+  else{
+    System.out.println("Tilt Left and Hold until further notice");
+    GetAll();
+      if (GyY>=OldGyY||AcY>=OldAcY){
+        System.out.println("Test Failed");
+      }
+      else{
+        System.out.println("Tilt Right and Hold until further notice");
+        GetAll();
+        if (GyY>=OldGyY||AcY>=OldAcY){
+            System.out.println("Test Failed");
+        }
+        else{
+          System.out.println("Test Passed");
+        }
+      }
+  }
+}
+
+    
