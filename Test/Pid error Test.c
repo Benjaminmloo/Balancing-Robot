20337894286b33ab5e5913f2214631379fb36f23
CarletonCalvinLam <32558238+CarletonCalvinLam@users.noreply.github.com>
2017-11-20 15:48:01 -0500

    Fixed formatting
    somehow the test fails even though the pid_out and correct have the exact same value and are both double

--- Test/Pid error Test.c
+++ Test/Pid error Test.c
@@ -1,25 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <Wire.h>
+//#include <Wire.h>
 
 const int SIGNAL_MAX = 255;//working PWM range: 100-255
 const int SIGNAL_MIN = 100;
 const int INITIAL_VALUE = 0;
+const int MEMORY_SIZE = 5;//threw error
 
 // PID parameters
 double Kp = 2.5;   // 2.5 //testing required to fine tune PID constants
 double Ki = 0.2;   // 1.0
 double Kd = 8.0;   // 8.0
 double K  = 1.9 * 1.12;
-
+double correct=103.633600;//value for testing
 
 // Complimentary Filter parameters
 double K0 = (double) 0.40;//percentage of gyroscopic value influence on final motor output 
 double K1 = (double) 0.60;//percentage of accelerometer/ rotation value's influence on final motor output
 
 int fd;
-int16_t acclX, acclY, acclZ;
-int16_t gyroX, gyroY, gyroZ;
+int acclX, acclY, acclZ;//originally uses int16_t for the arduino but not an available type for unit testing
+int gyroX, gyroY, gyroZ;//originally uses int16_t for the arduino but not an available type for unit testing
 double accl_scaled_x, accl_scaled_y, accl_scaled_z;
 double gyro_scaled_x, gyro_scaled_y, gyro_scaled_z;
 
@@ -33,8 +34,8 @@ double last_x, last_y;
 double GUARD_GAIN = 255;
 double error, last_error, integrated_error;
 double pTerm, iTerm, dTerm;
-int const memorySize = 5;
-double dMem[memorySize];
+
+double dMem[5];//originally MEMORY_SIZE but threw an error saying"error #2111: Integer expression must be constant."
 double dAvg;
 double angle;
 double angle_offset = -5;  //1.5
@@ -45,29 +46,20 @@ double dir;
 
 unsigned long timer, t, deltaT;
 
-void pid(){
+void pid(void){
+	//SETTING VALUES TO VALIDATE WITH
   error = 10;//instead of using last_x- angle offset, we use a manual value
   integrated_error=30;//completely arbitrary value for testing PID output
-  last_value=8;//arbitrary value for testing
+  last_error=8;//arbitrary value for testing
+
+
   pTerm = Kp * error;
   
   integrated_error = 0.95 * integrated_error + error;
   iTerm = Ki * integrated_error;
 
   dTerm = Kd * (error - last_error);
-  dAvg = dTerm;
-  //Serial.write("Values: "); Serial.print(dAvg);
-  for(int i = 0; i < memorySize - 1; i ++)
-  {
-    //Serial.write(" "); Serial.print(dMem[i]);
-    dAvg += dMem[i];
-    dMem[i] = dMem[i+1];
-  }
-  
-  dAvg += dMem[memorySize - 1];
-  dMem[memorySize - 1] = dTerm;
 
-  dAvg /= memorySize;
   //Serial.write("Average: "); Serial.print(dAvg); Serial.write("\n");
   last_error = error;
   
@@ -85,11 +77,21 @@ void pid(){
     speed = constrain(pid_out, -SIGNAL_MAX, -SIGNAL_MIN);//makes sure the output does not go beyond the allowable PWM range
   }*/
 }
-void test(){
-  if(pid_out==208.544){
-    System.out.println("Test Passed");
+void test(void){
+	printf("%f",correct);
+	printf("%f",pid_out);
+  if(pid_out==correct){
+    printf("Test Passed");
     }
   else{
-  System.out.println("Test Failed");
+		
+  	printf("Test Failed");
   }
 }
+int main(void){
+	pid();
+	test();
+	exit(0);
+	return 0;
+}
+
