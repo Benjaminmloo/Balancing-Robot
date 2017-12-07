25acbb19cd7d0b3ac3528e52be5f86de41e418a0
CarletonCalvinLam <32558238+CarletonCalvinLam@users.noreply.github.com>
2017-12-06 20:31:14 -0500

    Post code review
    removed unused code from original program that wasn't being tested, changed variables to constants, added comments to test and main methods

--- Test/Pid error Test.c
+++ Test/Pid error Test.c
@@ -1,97 +1,70 @@
 #include <stdio.h>
 #include <stdlib.h>
-//#include <Wire.h>
 
-const int SIGNAL_MAX = 255;//working PWM range: 100-255
-const int SIGNAL_MIN = 100;
-const int INITIAL_VALUE = 0;
-const int MEMORY_SIZE = 5;//threw error
+//working PWM range: 100-255
+const int SIGNAL_MAX = 255;//max value for pwm
+const int SIGNAL_MIN = 100;//minimum value for the pwm
+const int MEMORY_SIZE = 5;//use of the constant threw an error in line 39 but use of the value 5 itself is fine
 
 // PID parameters
-double Kp = 2.5;   // 2.5 //testing required to fine tune PID constants
-double Ki = 0.2;   // 1.0
-double Kd = 8.0;   // 8.0
-double K  = 1.9 * 1.12;
-double correct=103.633600;//value for testing
+//testing required to fine tune PID constants
+const double KP = 2.5;   // tuned Proportion value for use in PID controller
+const double KI = 0.2;   // tuned Integral Value for use in PID controller
+const double KD = 8.0;   // tuned Derivative Value for use in PID controller
+const double INTEGRATIONRATIO=0.96;//how much of the integrated error to be used
+const double K  = 1.9 * 1.12;//tuned K multiplier value for use in PID controller; output value =K*(Proportion value+ Integral Value+ Derivative Value)
+
 
 // Complimentary Filter parameters
-double K0 = (double) 0.40;//percentage of gyroscopic value influence on final motor output 
-double K1 = (double) 0.60;//percentage of accelerometer/ rotation value's influence on final motor output
+//double k0 = (double) 0.40;//percentage of gyroscopic value influence on final motor output 
+//double k1 = (double) 0.60;//percentage of accelerometer/ rotation value's influence on final motor output
 
-int fd;
-int acclX, acclY, acclZ;//originally uses int16_t for the arduino but not an available type for unit testing
-int gyroX, gyroY, gyroZ;//originally uses int16_t for the arduino but not an available type for unit testing
+//int fd;
+//originally uses int16_t for the arduino but not an available type for unit testing
+int acclX, acclY, acclZ;//accelerometer inputs from the "6DOF" in the xyz axis for control calculations
+int gyroX, gyroY, gyroZ;//gyroscope inputs in the xyz axis to be used for control calculations
 double accl_scaled_x, accl_scaled_y, accl_scaled_z;
 double gyro_scaled_x, gyro_scaled_y, gyro_scaled_z;
 
-
-double gyro_offset_x, gyro_offset_y;
-double gyro_total_x, gyro_total_y;
-double gyro_x_delta, gyro_y_delta;
-double rotation_x, rotation_y;
-double last_x, last_y;
-
-double GUARD_GAIN = 255;
 double error, last_error, integrated_error;
 double pTerm, iTerm, dTerm;
 
-double dMem[5];//originally MEMORY_SIZE but threw an error saying"error #2111: Integer expression must be constant."
-double dAvg;
-double angle;
-double angle_offset = -5;  //1.5
+double dMem[5];//originally used constant MEMORY_SIZE but threw an error saying"error #2111: Integer expression must be constant."
+//double dAvg;
+//double angle;
 
-double speed;
+//double speed;
 double pid_out;
-double dir;
-
-unsigned long timer, t, deltaT;
+double correct;//correct output value used in testing of the algorithm
 
 void pid(void){
 	//SETTING VALUES TO VALIDATE WITH
-  error = 10;//instead of using last_x- angle offset, we use a manual value
-  integrated_error=30;//completely arbitrary value for testing PID output
-  last_error=8;//arbitrary value for testing
+  	error = 10;//instead of using last_x- angle offset, we use a manual value
+  	integrated_error=30;//completely arbitrary value for testing PID output
+  	last_error=8;//arbitrary value for testing
 
+	correct=K*(KP*error+(INTEGRATIONRATIO*integrated_error+error)*KI+(error-last_error)*KD);//correct math for PID control to verify against
 
-  pTerm = Kp * error;
-  
-  integrated_error = 0.95 * integrated_error + error;
-  iTerm = Ki * integrated_error;
+  	pTerm = KP * error;//calculation of the P of PID
+  	integrated_error = INTEGRATIONRATIO * integrated_error + error;//replace with constant
+  	iTerm = KI * integrated_error;
+  	dTerm = KD * (error - last_error);
+  	last_error = error;
+  	pid_out = K * (pTerm + iTerm + dTerm);
 
-  dTerm = Kd * (error - last_error);
-
-  //Serial.write("Average: "); Serial.print(dAvg); Serial.write("\n");
-  last_error = error;
-  
-  pid_out = K * (pTerm + iTerm + dTerm);
-  //dir = pid_out / abs(pid_out);//direction dependent on sign of PID value
-
-  /*if(abs(pid_out) < 30)
-  {
-    speed = 0; //if the PID value is smaller than 30 it is negligible enough to not require rebalancing and thus a zero speed value is asserted
-  }else if(pid_out > 0) 
-  {
-    speed = constrain(pid_out, SIGNAL_MIN, SIGNAL_MAX);//makes sure the output does not go beyond the allowable PWM range
-  }else if(pid_out < 0)
-  {
-    speed = constrain(pid_out, -SIGNAL_MAX, -SIGNAL_MIN);//makes sure the output does not go beyond the allowable PWM range
-  }*/
 }
 void test(void){
-	printf("%f",correct);
-	printf("%f",pid_out);
-  if(pid_out==correct){
-    printf("Test Passed");
+
+  if(pid_out==correct){//Checking if the calculated value matches the correct one expected
+    printf("Test Passed\n");
     }
   else{
-		
-  	printf("Test Failed");
+  	printf("Test Failed\n");
   }
 }
 int main(void){
-	pid();
-	test();
+	pid();//calculation using the code being tested
+	test();//test the code generated result with what is correct
 	exit(0);
 	return 0;
 }
-
